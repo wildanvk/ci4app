ac13a0930152814c882c63326fd4a29210f2ee8e
wildanvk <wildanvinukananta@gmail.com>
2023-07-26 01:55:52 +0700

    Rename database to enterprise and provided the SQL

--- enterprise.sql
+++ enterprise.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 25, 2023 at 08:38 PM
+-- Generation Time: Jul 25, 2023 at 08:53 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `sig`
+-- Database: `enterprise`
 --
 
 -- --------------------------------------------------------
